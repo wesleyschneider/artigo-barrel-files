6499e784de05563057af4dce07d8018eb75c6df7
Wesley Schneider <wesleyschneideraires@gmail.com>
2023-11-27 11:34:26 -0300

    ✨ Configura Webpack e Tree shaking

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  mode: "development",
+};
